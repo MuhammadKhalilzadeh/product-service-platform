0a7355dc4c9604aa77816e42666caa87f5fdea47
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-02-11 23:33:42 +0330

    Binding user mocks' to controller and route

--- Server/controllers/user.ctrl.ts
+++ Server/controllers/user.ctrl.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from "express";
 import { READ_REAL_DATA } from "../flags";
+import { getAllMockUsers } from "../mock/utils/user.mock.util";
 
 export async function getAllUsers(req: Request, res: Response): Promise<any> {
   try {
     if (READ_REAL_DATA) {
     } else {
+      const users = getAllMockUsers();
+      res.status(200).json(users);
     }
-  } catch (error) {}
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
