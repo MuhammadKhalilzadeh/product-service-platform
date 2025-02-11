8cb54be4285a799ba595da17b60620d7966b00b1
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-02-11 23:38:12 +0330

    first get request, successful

--- Server/dist/index.js
+++ Server/dist/index.js
@@ -5,8 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+const user_route_1 = __importDefault(require("./routes/user.route"));
 const port = process.env.PORT || 3000;
+app.use(express_1.default.json());
 try {
+    app.use("/users", user_route_1.default);
     app.use("/", (req, res) => {
         res.json("Hello buddy!");
     });
