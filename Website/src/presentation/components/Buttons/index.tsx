3256f930547682ee0170a28fc34cf52ae1129b86
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-02-12 23:21:43 +0330

    progress on super admin register

--- Website/src/presentation/components/Buttons/index.tsx
+++ Website/src/presentation/components/Buttons/index.tsx
@@ -2,15 +2,32 @@ import { Button } from "@mui/material";
 
 interface TMButtonProps {
   label: string;
+  variant?: "text" | "outlined" | "contained" | undefined;
+  color?:
+    | "inherit"
+    | "primary"
+    | "secondary"
+    | "success"
+    | "error"
+    | "info"
+    | "warning"
+    | undefined;
+  onClick?: () => void;
 }
 
-const TMButton: React.FC<TMButtonProps> = ({ label }) => {
+const TMButton: React.FC<TMButtonProps> = ({
+  label,
+  variant = "contained",
+  color = "primary",
+  onClick,
+}) => {
   return (
     <Button
-      variant="contained"
+      variant={variant}
       disableElevation
-      color="primary"
+      color={color}
       sx={{ textTransform: "inherit" }}
+      onClick={onClick}
     >
       {label}
     </Button>
--- Website/src/presentation/pages/authentication/SuperAdminRegister/index.tsx
+++ Website/src/presentation/pages/authentication/SuperAdminRegister/index.tsx
@@ -2,8 +2,23 @@ import { Stack } from "@mui/material";
 import TMLogo from "../../../assets/logos/tm-logo-png-black.png";
 import TMTextField from "../../../components/Inputs/Textfield";
 import TMButton from "../../../components/Buttons";
+import { useState } from "react";
 
 const SuperAdminRegister = () => {
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    repeatPassword: "",
+  });
+
+  const handleChange =
+    (prop: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValues({ ...values, [prop]: event.target.value });
+    };
+
   return (
     <Stack
       sx={{
@@ -19,7 +34,8 @@ const SuperAdminRegister = () => {
           width: 360,
           border: "1px solid #cdcdcd",
           borderRadius: 2,
-          padding: 2,
+          paddingX: 2,
+          paddingY: 3,
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
@@ -28,12 +44,42 @@ const SuperAdminRegister = () => {
       >
         <img src={TMLogo} alt="TMLogo" style={{ width: 200 }} />
         <Stack sx={{ gap: 2 }}>
-          <TMTextField placeholder="First name" type="text" />
-          <TMTextField placeholder="Last name" type="text" />
-          <TMTextField placeholder="Email address" type="email" />
-          <TMTextField placeholder="Phone no." type="tel" />
-          <TMTextField placeholder="Password" type="password" />
-          <TMTextField placeholder="Repeat password" type="password" />
+          <TMTextField
+            placeholder="First name"
+            type="text"
+            value={values.firstName}
+            onChange={handleChange("firstName")}
+          />
+          <TMTextField
+            placeholder="Last name"
+            type="text"
+            value={values.lastName}
+            onChange={handleChange("lastName")}
+          />
+          <TMTextField
+            placeholder="Email address"
+            type="email"
+            value={values.email}
+            onChange={handleChange("email")}
+          />
+          <TMTextField
+            placeholder="Phone no."
+            type="tel"
+            value={values.phoneNumber}
+            onChange={handleChange("phone")}
+          />
+          <TMTextField
+            placeholder="Password"
+            type="password"
+            value={values.password}
+            onChange={handleChange("password")}
+          />
+          <TMTextField
+            placeholder="Repeat password"
+            type="password"
+            value={values.repeatPassword}
+            onChange={handleChange("repeatPassword")}
+          />
         </Stack>
         <TMButton label="Create Account" />
       </Stack>
